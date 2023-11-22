70fd9b6830d35273dbcf10e3e749640b84adaf09
Min12311 <cse1231@naver.com>
2023-11-22 10:48:49 +0900

    23112212

--- 3. 텍스트를 음성으로 변환/main3-1.py
+++ 3. 텍스트를 음성으로 변환/main3-1.py
@@ -1,14 +1,6 @@
 from gtts import gTTS
-class ttsEX:
-    def __init__(self):
-        self.text = "초기화상태"
-        self.FileName = "파일이름"
-    def makeSound(self):
-        tts = gTTS(text=self.text, lang='ko')
-        tts.save(f"{self.FileName}.mp3")
 
-if __name__ == '__main__':
-    t = ttsEX()
-    t.text = "3-1 tts 예제 입니다"
-    t.FileName = "테스트1"
-    t.makeSound()
+text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
+
+tts = gTTS(text=text, lang='ko')
+tts.save(r"3. 텍스트를 음성으로 변환\hi.mp3")
